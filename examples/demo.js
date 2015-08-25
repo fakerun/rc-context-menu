1a36c83a395785dffd9989a5b743c4a906d005cd
Ilya Dmitriev <fakefreerun@gmail.com>
2015-08-25 22:12:52 +0300

    Added some of demo function for testing purposes.

--- examples/demo.js
+++ examples/demo.js
@@ -1,5 +1,17 @@
 var demoApp = angular.module('demo', ['rc.contextMenu']);
 
 demoApp.controller("DemoController", [function() {
-    
+    var self = this;
+
+    self.firstActionSelected = function() {
+        console.log("first action selected");
+    };
+
+    self.secondActionSelected = function() {
+        console.log("second action selected");
+    };
+
+    self.thirdActionSelected = function() {
+        console.log("third action selected");
+    };
 }]);
